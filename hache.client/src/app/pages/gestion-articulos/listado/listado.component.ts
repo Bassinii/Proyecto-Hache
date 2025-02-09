9d36c1faec7242055fb97d55dd32ea8e4dda4bcc
Bruno Bassini <brunobassini86@gmail.com>
2025-02-08 23:40:14 -0300

    Add: DB connection in gestion-articulos component Enhance: gestion-articulo Style

--- hache.client/src/app/pages/gestion-articulos/listado/listado.component.ts
+++ hache.client/src/app/pages/gestion-articulos/listado/listado.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Articulo } from '../../../core/models/articulo';
+import { ArticuloServiceService } from '../../../core/services/articulo-service.service';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styleUrl: './listado.component.css'
 })
-export class ListadoComponent {
+export class ListadoComponent implements OnInit{
+  articulos: Articulo[] = [];
+
+  constructor(private articuloService_: ArticuloServiceService) { }
+
+  ngOnInit() {
+    this.obtenerArticulos();
+  }
+
+  obtenerArticulos() {
+    this.articuloService_.getArticulos().subscribe({
+      next: (data) => {
+        this.articulos = data;
+        console.log(this.articulos);
+      },
+      error: (error) => {
+        console.error('Error al obtener los articulos:', error);
+      }
+    })
+    this.articulos.sort((a, b) => a.categoria.id - b.categoria.id); //Ordena las categorias por id.
+
+  }
+
+
 
 }
