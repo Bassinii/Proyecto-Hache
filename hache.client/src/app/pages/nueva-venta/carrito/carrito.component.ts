43b152360a543df2c851af41629991fe10059070
Bruno Bassini <brunobassini86@gmail.com>
2025-02-20 14:42:42 -0300

    Add: Modal Checkout part 1

--- hache.client/src/app/pages/nueva-venta/carrito/carrito.component.ts
+++ hache.client/src/app/pages/nueva-venta/carrito/carrito.component.ts
@@ -1,5 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { ArticuloCarrito } from '../../../core/models/articulo-carrito';
+//import { Component, OnInit } from '@angular/core';
+//import { CarritoServiceService } from '../../../core/services/carrito-service.service';
+//import { FormsModule } from '@angular/forms';
+
+//@Component({
+//  selector: 'app-carrito',
+//  templateUrl: './carrito.component.html',
+//  styleUrls: ['./carrito.component.css']
+//})
+//export class CarritoComponent implements OnInit {
+
+//  // Propiedad para manejar el estado del modal
+//  mostrarModal: boolean = false;
+//  metodoPago: string = 'efectivo';
+//  pedidoYa: boolean = false;
+//  descuento: number = 0;
+
+//  constructor(private servicio: CarritoServiceService) { }
+
+//  ngOnInit(): void { }
+
+//  // Obtiene los artículos del carrito
+//  get carrito() {
+//    return this.servicio.getCarrito();
+//  }
+
+//  // Calcula el total
+//  get total() {
+//    return this.servicio.getTotal();
+//  }
+
+//  // Método para calcular el total con descuento
+//  calcularTotal() {
+//    const precioBase = this.total; // Usa el total del carrito como base
+//    return precioBase - (precioBase * (this.descuento / 100));
+//  }
+
+//  // Cierra el modal
+//  cerrarModal() {
+//    this.mostrarModal = false;
+//  }
+
+//  // Simula la confirmación de compra
+//  confirmarCompra() {
+//    console.log('Compra confirmada con:', this.metodoPago, this.pedidoYa, this.descuento);
+//    this.cerrarModal(); // Cierra el modal después de la compra
+//  }
+//}
+
+import { Component, Input, OnInit } from '@angular/core';
 import { CarritoServiceService } from '../../../core/services/carrito-service.service';
 
 @Component({
@@ -8,13 +56,12 @@ import { CarritoServiceService } from '../../../core/services/carrito-service.se
   styleUrl: './carrito.component.css'
 })
 export class CarritoComponent implements OnInit {
-  datos: any[] = [];
+
+  @Input() abrirModal!: () => void; // Recibe la función desde `NuevaVentaComponent`
 
   constructor(private servicio: CarritoServiceService) { }
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void { }
 
   get carrito() {
     return this.servicio.getCarrito();
@@ -23,5 +70,4 @@ export class CarritoComponent implements OnInit {
   get total() {
     return this.servicio.getTotal();
   }
-
 }
