ea99ffe2348c1dcfa295d756ba6cdf363d84466f
mateo barrios <mateobabarrios@gmail.com>
2025-02-23 13:18:29 -0300

    Add: logout and modifications in the navbar

--- hache.client/src/app/shared/components/nav-bar/nav-bar.component.ts
+++ hache.client/src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiceAuthService } from '../../../auth/serviceAuth/service-auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'nav-bar',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(public authService: ServiceAuthService, private router: Router) { }
 
   ngOnInit(): void { }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
 
